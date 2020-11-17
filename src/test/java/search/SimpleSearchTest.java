7ea357c266cd3f083f1a4c5db1c86cc29b728865
Adi Tofan <adi_tofan@yahoo.com>
2020-11-17 15:04:10 +0200

    update

--- src/test/java/search/SimpleSearchTest.java
+++ src/test/java/search/SimpleSearchTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class SimpleSearchTest {
 
@@ -15,6 +21,19 @@ public class SimpleSearchTest {
 
         driver.get("http://testfasttrackit.info/selenium-test/");
 
-        driver.findElement(By.id("search")).sendKeys("vase" + Keys.ENTER);
+        String searchKeyword = "vase";
+        driver.findElement(By.id("search")).sendKeys(searchKeyword + Keys.ENTER);
+
+        System.out.println("Pressed Enter");
+
+        List<WebElement> productNames = driver.findElements(By.cssSelector("h2.product-name a"));
+
+        System.out.println("Stored " + productNames.size() + " product names");
+
+        for (WebElement productName : productNames) {
+            assertThat("Some product names do not contain the keyword",
+                    productName.getText(), containsString(searchKeyword.toUpperCase()));
+        }
+
     }
 }
