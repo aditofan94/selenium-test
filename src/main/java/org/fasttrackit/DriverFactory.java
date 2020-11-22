84c1823db532487cfe476e4ca131b5f483b20928
Adi Tofan <adi_tofan@yahoo.com>
2020-11-22 17:04:09 +0200

    update 3

--- src/main/java/org/fasttrackit/DriverFactory.java
+++ src/main/java/org/fasttrackit/DriverFactory.java
@@ -30,7 +30,7 @@ public class DriverFactory {
         }
 
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
 
         return driver;
